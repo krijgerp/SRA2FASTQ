8bd3b6c45c7294619527ba07b6737f72721c5422
Peter Krijger <40625591+krijgerp@users.noreply.github.com>
2022-11-29 09:57:09 +0100

    Update SRA2FASTQ.sh

--- SRA2FASTQ.sh
+++ SRA2FASTQ.sh
@@ -1,15 +1,26 @@
-#conda create -n SRA -c bioconda sra-tools
+!/bin/bash
+## This script download SRA files from NCBI and convert them to fastq
+## The script requires a list of SRR IDs (SRR_ID.tsv) and a conda environment (SRA) with the SRA toolkit installed
+
+## Activate conda environment
 conda activate SRA
+
+## Create a directory to store SRA files
 mkdir ./SRA_download
+
+## Loop over the SRR IDs
 for s in $(cat SRR_ID.tsv | sed 's/\t/,/g' | grep "SRA" -v)
 do 
+## Extract the name and ID from the list
 name=$(echo $s | cut -d "," -f 1)
 ID=$(echo $s | cut -d "," -f 2)
+
 echo -e "id\t$ID ======================" 2>&1 | tee -a SRRdump.log \
 && prefetch $ID --max-size 100G -O ./SRA_download/ 2>&1 | tee -a SRRdump.log \
 && vdb-validate ./SRA_download/$ID  2>&1 | tee -a SRRdump.log \
 && fasterq-dump ./SRA_download/$ID --outdir ./fastq/ --threads 8 --progress  | tee -a SRRdump.log  \
-&& pigz -c -p 8 ./fastq/$ID.fastq > ./fastq/$name_$ID.fastq.gz \
-&& rm -R ./fastq/$ID.fastq
+&& echo "Compressing sample $ID" \
+&& pigz -c -p 8 ./fastq/$ID.fastq > ./fastq/$name.$ID.fastq.gz \
+&& rm -R ./fastq/$ID.fastq \
+&& rm -R ./SRA_download/$ID
 done
-rm -R ./SRA_download
